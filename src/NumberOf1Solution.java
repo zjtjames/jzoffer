0b522f48d8fd8a9870ff5cf08f3219ec9f0561e7
uzig305 <zjt_james@163.com>
2019-02-22 00:04:22 +0800

    二进制中1的个数

---
+++ src/NumberOf1Solution.java
@@ -0,0 +1,19 @@
+/**
+ * created by Zheng Jiateng on 2019/2/22.
+ */
+
+/**
+ * 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+ */
+public class NumberOf1Solution {
+    public int NumberOf1(int n) {
+        int count = 0;
+        while(n != 0){
+            if((n&1) == 1){
+                count++;
+            }
+            n = n >>> 1;
+        }
+        return count;
+    }
+}
--- src/ReplaceSpaceSolution.java
+++ src/ReplaceSpaceSolution.java
@@ -2,6 +2,9 @@
  * created by Zheng Jiateng on 2019/2/21.
  */
 
+/**
+ * 请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+ */
 public class ReplaceSpaceSolution {
     public String replaceSpace(StringBuffer str) {
         String origin = str.toString();
