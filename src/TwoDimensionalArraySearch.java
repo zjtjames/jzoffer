0659b3dc4cce680dcc72331cd7c5c8a21b7c84d9
uzig305 <zjt_james@163.com>
2019-02-22 18:03:56 +0800

    二维数组中的查找

---
+++ src/TwoDimensionalArraySearch.java
@@ -0,0 +1,23 @@
+/**
+ * created by Zheng Jiateng on 2019/2/22.
+ */
+
+/*
+在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ */
+public class TwoDimensionalArraySearch {
+    public boolean Find(int target, int [][] array) {
+        // 行数m 列数n 总数q
+        int q = array.length * array[0].length;
+        int i =
+        while (target != )
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int i = 3;
+        System.out.println(i/2);
+    }
+}
