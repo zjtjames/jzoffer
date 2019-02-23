43d843b0cc0880fbf4b19c92373ea3d797e11e42
James Zheng <zjtjames@gmail.com>
2019-02-23 14:05:43 +0800

    变态跳台阶

---
+++ src/JumpFloorII.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Zheng Jiateng on 2019/2/23.
+ */
+
+/**
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。
+ * 求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ */
+public class JumpFloorII {
+    public int JumpFloorII(int target) {
+        if (target == 1) {
+            return 1;
+        } else {
+            int sum = 0;
+            for (int i = 1; i < target; i++) {
+                sum += JumpFloorII(i);
+            }
+            return sum + 1;
+        }
+    }
+}
