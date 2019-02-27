fe8a14add93394fb1e2025a32e1a7583951b134b
uzig305 <zjt_james@163.com>
2019-02-27 20:47:35 +0800

    链表中倒数第k个节点

--- src/FindKthToTail.java
+++ src/FindKthToTail.java
@@ -2,8 +2,11 @@
  * created by Zheng Jiateng on 2019/2/27.
  */
 
+
+import java.util.ArrayList;
+
 /**
- * 链表中倒数第k个节点
+ * 链表中倒数第k个节点 : 代码的鲁棒性
  * 输入一个链表，输出该链表中倒数第k个结点。
  */
 public class FindKthToTail {
@@ -18,7 +21,18 @@ public class FindKthToTail {
     }
 
     public ListNode findKthToTail(ListNode head,int k) {
+        if (head != null) {
+            ArrayList<ListNode> list = new ArrayList<>();
+            while (head.next != null) {
+                list.add(head);
+                head = head.next;
+            }
+            list.add(head);
+            if (list.size() >= k && k > 0) {
 
+                return list.get(list.size() - k);
+            }
+        }
+        return null;
     }
-
 }
