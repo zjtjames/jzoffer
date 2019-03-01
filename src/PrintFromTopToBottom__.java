518d820ff126d3f81df5e5e3b29f7eb3fac9baa5
uzig305 <zjt_james@163.com>
2019-03-01 22:52:06 +0800

    从上往下打印二叉树

---
+++ src/PrintFromTopToBottom__.java
@@ -0,0 +1,43 @@
+/**
+ * created by Zheng Jiateng on 2019/3/1.
+ */
+
+import java.util.ArrayList;
+
+/**
+ * 从上往下打印二叉树 ：层序遍历 用队列实现
+ * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+ */
+public class PrintFromTopToBottom__ {
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+    }
+
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<Integer>();
+        }
+        ArrayList<TreeNode> queue = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            root = queue.remove(0);
+            result.add(root.val);
+            if (root.left != null) {
+                queue.add(root.left);
+            }
+            if (root.right != null) {
+                queue.add(root.right);
+            }
+        }
+        return result;
+    }
+}
