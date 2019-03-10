a061baa27c8c6ff19f934813b9e9f33ed0182fd9
uzig305 <zjt_james@163.com>
2019-03-10 13:27:09 +0800

    doc

--- src/PrintMinNumber__.java
+++ src/PrintMinNumber__.java
@@ -12,7 +12,7 @@ import java.util.Comparator;
  * 输入一个正整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
  * 例如输入数组{3，32，321}，则打印出这三个数字能排成的最小数字为321323。
  */
-public class PrintMinNumber {
+public class PrintMinNumber__ {
     public String PrintMinNumber(int [] numbers) {
         if (numbers == null || numbers.length == 0) {
             return "";
