905cd4505f59d82bed15f404b04fefa89577025f
uzig305 <zjt_james@163.com>
2019-03-12 23:57:53 +0800

    翻转单词顺序列

---
+++ src/ReverseSentence.java
@@ -0,0 +1,33 @@
+/**
+ * created by Zheng Jiateng on 2019/3/12.
+ */
+
+/**
+ * 翻转单词顺序列 : String[] strs = " ".split(" ")  strs的长度为0
+ *
+ * 牛客最近来了一个新员工Fish，每天早晨总是会拿着一本英文杂志，写些句子在本子上。
+ * 同事Cat对Fish写的内容颇感兴趣，有一天他向Fish借来翻看，但却读不懂它的意思。
+ * 例如，“student. a am I”。后来才意识到，这家伙原来把句子单词的顺序翻转了，正确的句子应该是“I am a student.”。
+ * Cat对一一的翻转这些单词顺序可不在行，你能帮助他么？
+ */
+public class ReverseSentence {
+    public String ReverseSentence(String str) {
+        String[] strs = str.split(" ");
+        if (strs.length == 0) {
+            return str;
+        }
+        String result = "";
+        for (int i = strs.length - 1; i >= 0; i--) {
+            result += strs[i];
+            result += " ";
+        }
+        return result.trim();
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(new ReverseSentence().ReverseSentence("I am a student."));
+        String str = " ";
+        String[] strs = str.split(" ");
+        System.out.println(strs.length);
+    }
+}
