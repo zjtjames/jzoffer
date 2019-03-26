3a283a621197a70f41fd12aee83ee3c5cf7e5d38
uzig305 <zjt_james@163.com>
2019-03-26 22:28:10 +0800

    插入排序

---
+++ src/sort/Insertion.java
@@ -0,0 +1,31 @@
+/**
+ * created by Zheng Jiateng on 2019/3/26.
+ */
+package sort;
+
+/**
+ * 插入排序 ：O(n方)
+ * 将a[i]与a[0]到a[i-1]中比它大的所有元素依次有序地交换。
+ *
+ * 插入排序对部分有序的数组十分高效，也很适合小规模数组
+ */
+public class Insertion {
+    public static void sort(int[] a) {
+        int n = a.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j > 0 && a[j] < a[j - 1]; j--) {
+                int tmp = a[j];
+                a[j] = a[j - 1];
+                a[j - 1] = tmp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 6, 5, 4, 7, 8};
+        sort(a);
+        for (int num : a) {
+            System.out.print(num + " ");
+        }
+    }
+}
