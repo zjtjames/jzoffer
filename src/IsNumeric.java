cac2d1be4b6bedaa1167dfcb1333cf5af968d5db
uzig305 <zjt_james@163.com>
2019-04-03 14:50:09 +0800

    表示数值的字符串 正则表达式

--- src/IsNumeric.java
+++ src/IsNumeric.java
@@ -7,15 +7,18 @@
  *
  * 请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。
  * 例如，字符串"+100","5e2","-123","3.1416"和"-1E-16"都表示数值。
+ *
  * 但是"12e","1a3.14","1.2.3","+-5"和"12e+4.3"都不是。
  */
 public class IsNumeric {
     public boolean isNumeric(char[] str) {
         String string = String.valueOf(str);
-        return string.matches("");
+        return string.matches("[+\\-]?\\d*(\\.\\d+)?([eE][+\\-]?\\d+)?");
     }
 
     public static void main(String[] args) {
-        System.out.println("1\\2");
+        String string = "";
+        char[] chars = string.toCharArray();
+        System.out.println(new IsNumeric().isNumeric(chars));
     }
 }
