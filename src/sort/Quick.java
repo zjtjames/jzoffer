4d5d69551320002edaebe9d87f879922daec8cca
uzig305 <zjt_james@163.com>
2019-07-23 20:16:39 +0800

    更好理解的快排

--- src/sort/Quick.java
+++ src/sort/Quick.java
@@ -6,58 +6,46 @@ package sort;
 
 /**
  * 快速排序： O(nlogn) 分治 原地排序 不需要辅助空间
- * 当两个子数组都有序时数组也就自然有序了，递归发生在处理整个数组（partition）之后
+ * 快速排序,说白了就是给基准数据找其正确索引位置的过程
  *
  */
 public class Quick {
 
-    public static void sort(int[] a) {
-        sort(a, 0, a.length - 1);
+    private static void quickSort(int[] nums, int lo, int hi) {
+
+        if (lo < hi) {
+            // 找寻基准数据的正确索引
+            int index = findIndex(nums, lo, hi);
+
+            // 进行迭代对index之前和之后的数组进行相同的操作使整个数组变成有序
+            quickSort(nums, lo, index - 1);
+            quickSort(nums, index + 1, hi);
+        }
     }
 
-    private static void sort(int[] a, int lo, int hi) {
-        if (lo >= hi) {
-            return;
+    private static int findIndex(int[] nums, int lo, int hi) {
+        // 基准数据
+        int tmp = nums[lo];
+        while (lo < hi) {
+            // 当队尾的元素大于等于基准数据时,向前挪动high指针
+            while (lo < hi && nums[hi] >= tmp) hi--;
+            // 如果队尾元素小于tmp了,需要将其赋值给low
+            nums[lo] = nums[hi];
+            // 当队首元素小于等于tmp时,向后挪动low指针
+            while (lo < hi && nums[lo] <= tmp) lo++;
+            // 当队首元素大于tmp时,需要将其赋值给high
+            nums[hi] = nums[lo];
         }
-        int j = partition(a, lo, hi);
-        sort(a, lo, j - 1);
-        sort(a, j + 1, hi);
-    }
-
-    // 每次partition会排定一个元素，所以至少数组要有两个元素才能partition 所以sort中递归终止条件为lo >= hi
-    // 一般先选择a[lo]作为切分元素，即那个将会被排定的元素
-    private static int partition(int[] a, int lo, int hi) {
-        int i = lo; // 扫描左指针
-        int j = hi + 1; // 扫描右指针
-        int v = a[lo];
-        while (true) {
-            while (a[++i] < v) { // 直到左边一个比v大的
-                if (i == hi) {
-                    break;
-                }
-            }
-            while (a[--j] > v) { // 直到右边一个比v小的
-                if (j == lo) {
-                    break;
-                }
-            }
-            if (j <= i) {
-                break;
-            }
-            int tmp = a[i]; // 交换左边大右边小的两个元素 使得循环结束时，排定的元素左边都比该元素小，右边都比该元素大
-            a[i] = a[j];
-            a[j] = tmp;
-        }
-        int tmp = a[lo];
-        a[lo] = a[j];
-        a[j] = tmp;
-        return j; // a[lo..j-1] <= a[j] <= a[j+1..hi]达成
+        // 跳出循环时low和high相等,此时的low或high就是tmp的正确索引位置
+        // 由原理部分可以很清楚的知道low位置的值并不是tmp,所以需要将tmp赋值给arr[low]
+        nums[lo] = tmp;
+        return lo; // 返回tmp的正确位置
     }
 
     public static void main(String[] args) {
-        int[] a = {5, 6, 7, 8, 1, 2, 3, 4};
-        sort(a);
-        for (int num : a) {
+        int[] nums = {5, 6, 7, 8, 1, 2, 3, 4};
+        quickSort(nums, 0, nums.length - 1);
+        for (int num : nums) {
             System.out.println(num);
         }
     }
